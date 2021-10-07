c83da013542df0116052c58edc0725627a13388a
조민수 <chominsu10@gmail.com>
2021-10-07 11:32:12 +0900

    [SET] recoil

--- k-bank/pages/_app.js
+++ k-bank/pages/_app.js
@@ -1,7 +1,13 @@
-import "../styles/globals.css";
+import GlobalStyle from "../styles/GlobalStyle.js";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
 
 export default MyApp;
